99c7f810fb0a67a17a06df8caecdd80d41411c00
HMBSbige <admin@bige0.com>
2018-05-13 12:43:02 +0800

    Move from AdminButtonDemo

---
+++ AdminButtonDemo/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AdminButtonDemo
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+
+			if (!AdminButton.IsAdmin())
+			{
+				Text += @"（标准用户权限）";
+				AdminButton.AddShieldToButton(button1);
+			}
+			else
+			{
+				Text += @"（管理员权限）";
+			}
+		}
+
+		public sealed override string Text
+		{
+			get => base.Text;
+			set => base.Text = value;
+		}
+
+		private void DoAdminTask()
+		{
+			try
+			{
+				var fileName = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)
+							   + @"\Microsoft\Windows\Start Menu\TEST.BG";
+				if (System.IO.File.Exists(fileName))
+				{
+					System.IO.File.Delete(fileName);
+					MessageBox.Show(@"TEST.BG 删除成功");
+				}
+				else
+				{
+					System.IO.File.Create(fileName).Close();
+					MessageBox.Show(@"TEST.BG 创建成功");
+				}
+			}
+			catch (UnauthorizedAccessException uacEx)
+			{
+				MessageBox.Show(uacEx.Message, uacEx.Data.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, ex.Data.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (AdminButton.IsAdmin())
+			{
+				DoAdminTask();
+			}
+			else
+			{
+				AdminButton.RestartElevated();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			DoAdminTask();
+		}
+	}
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,95 +1,48 @@
 ﻿using System;
 using System.Diagnostics;
+using Common;
 
 namespace CSharp
 {
 	internal class Program
 	{
-		private static int[][] JaggedArrayCreate()
-		{
-			var a = new int[9][];
-			for (var i = 0; i < 9; ++i)
-			{
-				a[i] = new int[9];
-			}
-			return a;
-		}
-
-		private static void JaggedArrayWrite(ref int[][] a)
-		{
-			for (var i = 0; i < 9; ++i)
-			{
-				for (var j = 0; j < 9; ++j)
-				{
-					a[i][j] = 1;
-				}
-			}
-		}
-
-		private static int[,] MultidimensionalArrayCreate()
-		{
-			var b = new int[9, 9];
-			return b;
-		}
-
-		private static void MultidimensionalArrayWrite(ref int[,] b)
-		{
-			for (var i = 0; i < 9; ++i)
-			{
-				for (var j = 0; j < 9; ++j)
-				{
-					b[i, j] = 1;
-				}
-			}
-		}
-
-		private delegate void VoidMethod();
-
-		private static double BenchMark(VoidMethod method)
-		{
-			var stopwatch = new Stopwatch();
-			stopwatch.Start();
-			method();
-			stopwatch.Stop();
-			return stopwatch.ElapsedTicks / (double) Stopwatch.Frequency;
-		}
-
 		static void Main(string[] args)
 		{
-			var a = JaggedArrayCreate();
-			var b = MultidimensionalArrayCreate();
+			var a = Benchmark.JaggedArrayCreate();
+			var b = Benchmark.MultidimensionalArrayCreate();
 			const int n = 10000000;
+
 			//Jagged Arrays
-			Console.WriteLine(@"交错数组创建：" + BenchMark(() =>
+			Console.WriteLine(@"交错数组创建：" + Benchmark.SW(() =>
 			{
 				for (var i = 0; i < n; ++i)
 				{
-					a = JaggedArrayCreate();
+					a = Benchmark.JaggedArrayCreate();
 				}
 			}));
 
-			Console.WriteLine(@"交错数组写：" + BenchMark(() =>
+			Console.WriteLine(@"交错数组写：" + Benchmark.SW(() =>
 			{
 				for (var i = 0; i < n; ++i)
 				{
-					JaggedArrayWrite(ref a);
+					Benchmark.JaggedArrayWrite(ref a);
 				}
 			}));
 
 			//Multidimensional Arrays
-			Console.WriteLine(@"二维数组创建：" + BenchMark(() =>
+			Console.WriteLine(@"二维数组创建：" + Benchmark.SW(() =>
 			{
 				for (var i = 0; i < n; ++i)
 				{
-					b = MultidimensionalArrayCreate();
+					b = Benchmark.MultidimensionalArrayCreate();
 				}
 			}));
 
-			Console.WriteLine(@"二维数组写：" + BenchMark(() =>
+			Console.WriteLine(@"二维数组写：" + Benchmark.SW(() =>
 			{
 				for (var i = 0; i < n; ++i)
 				{
-					MultidimensionalArrayWrite(ref b);
+					Benchmark.MultidimensionalArrayWrite(ref b);
 				}
 			}));
 		}
