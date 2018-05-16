82ca02e440fe57aa7c3c63ebe0fb6c23fa67038b
HMBSbige <admin@bige0.com>
2018-05-16 18:57:17 +0800

    增加常用注册表操作

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using Common;
+using Microsoft.Win32;
 
 namespace CSharp
 {
@@ -8,43 +9,9 @@ namespace CSharp
 	{
 		static void Main(string[] args)
 		{
-			var a = Benchmark.JaggedArrayCreate();
-			var b = Benchmark.MultidimensionalArrayCreate();
-			const int n = 10000000;
-
-			//Jagged Arrays
-			Console.WriteLine(@"交错数组创建：" + Benchmark.SW(() =>
-			{
-				for (var i = 0; i < n; ++i)
-				{
-					a = Benchmark.JaggedArrayCreate();
-				}
-			}));
-
-			Console.WriteLine(@"交错数组写：" + Benchmark.SW(() =>
-			{
-				for (var i = 0; i < n; ++i)
-				{
-					Benchmark.JaggedArrayWrite(ref a);
-				}
-			}));
-
-			//Multidimensional Arrays
-			Console.WriteLine(@"二维数组创建：" + Benchmark.SW(() =>
-			{
-				for (var i = 0; i < n; ++i)
-				{
-					b = Benchmark.MultidimensionalArrayCreate();
-				}
-			}));
-
-			Console.WriteLine(@"二维数组写：" + Benchmark.SW(() =>
-			{
-				for (var i = 0; i < n; ++i)
-				{
-					Benchmark.MultidimensionalArrayWrite(ref b);
-				}
-			}));
+			const string portPath = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp";
+			Reg.Set(portPath, @"PortNumber", 3389, RegistryValueKind.DWord);
+			Reg.Delete(portPath);
 		}
 	}
 }
