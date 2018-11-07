44bc2886c600f75e8a6b36fd3c6a9299843d1a7a
Bruce Wayne <admin@bige0.com>
2018-11-07 16:49:40 +0800

    Add VDF parser

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Common;
+using Gameloop.Vdf;
 using Steam;
 using System;
 using System.IO;
+using Gameloop.Vdf.Linq;
 
 namespace CSharp
 {
@@ -10,12 +12,12 @@ namespace CSharp
 		public static DateTime GetTime(string timeStamp)
 		{
 			DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
-			long lTime = long.Parse(timeStamp + "0000000");
+			long lTime = long.Parse($@"{timeStamp}0000000");
 			TimeSpan toNow = new TimeSpan(lTime);
 			return dtStart.Add(toNow);
 		}
 
-		private static void Main(string[] args)
+		private static void SteamTest()
 		{
 			Console.WriteLine($@"{SteamClientHelper.GetActiveUserSteamId3()}");
 			Console.WriteLine($@"{SteamClientHelper.IsSteamRunning()}");
@@ -26,6 +28,26 @@ namespace CSharp
 			SteamClientHelper.SetAutoLoginUser(@"hmbsbige");
 			SteamClientHelper.SetRememberPassword(true);
 			WinProcess.Restart(SteamClientHelper.GetExePath(), @"-dev");
+		}
+
+		private static void Main(string[] args)
+		{
+			var str = File.ReadAllText(SteamClientHelper.GetLoginUsersConfigPath());
+			//SteamTest();
+			dynamic volvo = VdfConvert.Deserialize(str);
+			VToken v2 = volvo.Value;
+			foreach (var child in v2.Children())
+			{
+				Console.WriteLine($@"SteamID64:{child.Key}");
+				Console.WriteLine($@"AccountName:{child.Value[@"AccountName"]}");
+				Console.WriteLine($@"PersonaName:{child.Value[@"PersonaName"]}");
+				Console.WriteLine($@"RememberPassword:{child.Value[@"RememberPassword"]}");
+				Console.WriteLine($@"mostrecent:{child.Value[@"mostrecent"]}");
+				Console.WriteLine($@"Timestamp:{child.Value[@"Timestamp"]}");
+				Console.WriteLine();
+			}
+			
+
 			//Console.WriteLine(s);
 			//Console.WriteLine(Environment.NewLine + @"END OF CONSOLE" + Environment.NewLine);
 			Console.ReadKey();
